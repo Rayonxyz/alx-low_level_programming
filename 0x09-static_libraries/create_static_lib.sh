57446355d0544af541e864dc6db52781121cfe38
Rayonxyz <101402187+Rayonxyz@users.noreply.github.com>
2022-05-05 10:22:50 +0000

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
-ranlib liball.a
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}
