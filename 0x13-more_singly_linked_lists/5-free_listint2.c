46c460f862b390ac6a70822f2197148e9b051204
Rayonxyz <101402187+Rayonxyz@users.noreply.github.com>
2022-05-30 14:13:25 +0000

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,1 +1,26 @@
+#include "lists.h"
 
+/**
+ * main - Entry point.
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
+
+}
