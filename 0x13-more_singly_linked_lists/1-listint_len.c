4bf26b9ef50ad198afbf3c45f7a9cc21631f8f04
Rayonxyz <101402187+Rayonxyz@users.noreply.github.com>
2022-05-30 13:42:31 +0000

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
 
+/**
+ * main - Entry point .
+ *
+ *
+ * Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t nbr_element = 0;
+	const listint_t *current = h;
+
+	while (current != NULL)
+	{
+		current = current->next;
+		nbr_element++;
+	}
+	return (nbr_element);
+}
