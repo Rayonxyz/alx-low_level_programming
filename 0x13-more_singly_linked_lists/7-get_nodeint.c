16aa2476e88545889cca8b2f4aa12cfe874b9e65
Rayonxyz <101402187+Rayonxyz@users.noreply.github.com>
2022-05-30 14:33:47 +0000

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,1 +1,28 @@
+#include "lists.h"
 
+/**
+ * main - Entry point.
+ * 
+ * 
+ * Return: node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *current;
+
+	if (head == NULL)
+		return (0);
+
+	current = head;
+
+	while (index != 0)
+	{
+		current = current->next;
+		index--;
+		if (current == NULL)
+			return (0); /*Out of range*/
+	}
+
+	return (current);
+}
