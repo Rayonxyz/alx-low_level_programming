47cb47c155cc21b1fa3808ecac870a8a75a9fdf4
Rayonxyz <101402187+Rayonxyz@users.noreply.github.com>
2022-05-30 14:04:02 +0000

    Create 4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * main - Entry point.
+ * 
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+	listint_t *next;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
