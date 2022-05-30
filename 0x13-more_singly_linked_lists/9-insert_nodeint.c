c22b5263e6a283d9753db4bb681e487b6e22a686
Rayonxyz <101402187+Rayonxyz@users.noreply.github.com>
2022-05-30 14:54:47 +0000

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,1 +1,49 @@
+#include "lists.h"
+
+/**
+ * main - Entry point.
+ * 
+ * 
+ * 
+ * Return: new node
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *current;
+	listint_t *new;
+
+	if (head == NULL)
+		return (0);
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (0);
+
+	new->next = NULL;
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		(*head) = new;
+		return (new);
+	}
+
+	current = *head;
+
+	while (idx != 1)
+	{
+		current = current->next;
+		--idx;
+		if (current == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
+	}
+	new->next = current->next;
+	current->next = new;
+
+	return (new);
+}
 
